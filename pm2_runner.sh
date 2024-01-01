262376de69ef2c669af98d643906f830578cf408
ABU SAID <mdabusaidriyaz@gmail.com>
2024-01-01 22:56:51 +0600

    Update pm2_runner.sh

--- pm2_runner.sh
+++ pm2_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! type pm2 > /dev/null
 then
-  sudo npm install -g pm2 && pm2 start npm --name nextjs-app -- run start -- -p 3000
+  sudo npm install -g pm2 && sudo pm2 start npm --name nextjs-app -- run start -- -p 3000
 else
-  pm2 restart nextjs-app
+  sudo pm2 restart nextjs-app
 fi
